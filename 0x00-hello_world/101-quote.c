b0cfc29d539f7aa0caf49ff262e477df2420ae0f
mrprinceikpa <obinichi2002@gmail.com>
2022-09-08 19:27:53 -0700

    version with mgc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - the reference funtion for the sourcecode
  *Return: returns 1
  */
 int main(void)
 {
-	char oput[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, oput, 59);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
