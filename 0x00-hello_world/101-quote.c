e356d4a1d07f730f9a564579e32089b08d40baab
mrprinceikpa <obinichi2002@gmail.com>
2022-09-08 19:31:00 -0700

    version with mgc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  *main - the reference funtion for the sourcecode
  *Return: returns 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 1, 59, stderr);
 	return (1);
 }
