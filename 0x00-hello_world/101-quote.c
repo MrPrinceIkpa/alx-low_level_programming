6eae187edf629361d9d549472bf2775d2cbe99d4
mrprinceikpa <obinichi2002@gmail.com>
2022-09-08 18:38:06 -0700

    version with mgc

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - the reference funtion for the sourcecode
+ *Return: returns 1
+ */
+int main(void)
+{
+	char oput[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(oput, 1, sizeof(oput), stderr);
+
+	return (1);
+}
