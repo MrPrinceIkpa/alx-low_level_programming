9365c01dee0e6fe8047dbd9717939f5210184424
mrprinceikpa <obinichi2002@gmail.com>
2022-09-08 19:09:04 -0700

    version with mgc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char oput[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char oput[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fwrite(oput, 1, sizeof(oput), stderr);
 	return (1);
