8db26fcef82bcaa2d561bed84a7d1368708f088e
mrprinceikpa <obinichi2002@gmail.com>
2022-09-08 17:10:44 -0700

    Tasks on the C programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - It is the reference function for this source code
+ * Return: It returns an integer
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
