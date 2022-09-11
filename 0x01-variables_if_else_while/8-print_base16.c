6a5db0ed511b393ec34729f54229c1af0aa57dbe
mrprinceikpa <obinichi2002@gmail.com>
2022-09-11 10:09:56 -0700

    C projects- flow control and loops

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- *main prints numbers of base 16
+ *main - prints numbers of base 16
  *Return: returns 0 on completion
  */
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 		putchar(i);
-	for (i = 97; i < 103; i++ )
+	for (i = 97; i < 103; i++)
 		putchar(i);
 	putchar(10);
 	return (0);
