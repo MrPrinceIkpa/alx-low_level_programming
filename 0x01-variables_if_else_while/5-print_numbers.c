50d22145323654702844f77e39594867b36b6293
mrprinceikpa <obinichi2002@gmail.com>
2022-09-11 08:26:05 -0700

    Project in C- flow control and loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints out single digits in base 10
+ *Return: returns 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
