818cd25bc331de01f3830c0a6aff0df3d30af5cc
mrprinceikpa <obinichi2002@gmail.com>
2022-09-11 09:43:39 -0700

    C projects- flow control and loops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print single digits
+ *Return: returns 0 on completion
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar(10);
+	return (10);
+}
