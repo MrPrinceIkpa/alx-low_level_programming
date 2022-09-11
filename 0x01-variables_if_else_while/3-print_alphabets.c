0607a30dfcb594fbb72db7373092554a02cf7b93
mrprinceikpa <obinichi2002@gmail.com>
2022-09-11 07:46:34 -0700

    C projects- flow control and loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabets
+ *Description: It prints both the lower and upper case letters
+ *Return: returns 0 on completion
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar(10);
+}
