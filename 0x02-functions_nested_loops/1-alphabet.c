c69e748e8946b574ec42d8f1bd4b444e20d683a1
mrprinceikpa <obinichi2002@gmail.com>
2022-09-13 13:06:08 -0700

    C projects- Functions and Nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,16 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+/**
+ *print_alphabet - does the actual printing of the alpahbets
+ *Return: return is void
+ */
+void print_alphabet(void)
+{
+	char abet;
+
+	for (abet = 97; abet < 123; abet++)
+		_putchar(abet);
+	_putchar(10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,20 +5,10 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 /**
  *print_alphabet - does the actual printing of the alpahbets
  *Return: return is void
  */
-void print_alphabet(void)
-{
-	char abet;
-
-	for (abet = 97; abet < 123; abet++)
-		_putchar(abet);
-	_putchar(10);
-}
+void print_alphabet(void);
