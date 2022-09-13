3d1259b9dd261698f2644e4b457eacda7c82a85d
mrprinceikpa <obinichi2002@gmail.com>
2022-09-13 13:56:29 -0700

    C projects- Fuctions and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,16 @@
 
 /**
  *_islower - cheacks if a char is lowercase
+ *@c: the character which is been tested. Expected
+ * be of the datatype integer. 
  (*An assummption that the machine uses ASCII character set
+ )* is made
  *Return: returns 1 if lowercase, otherwise returns 0
  */
 int _islower(int c)
 {
 	if (c < 97 || c > 122)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 }
