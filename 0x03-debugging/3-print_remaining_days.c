04ff8ec6e22ed020d9fd55739752a20217f67234
mrprinceikpa <obinichi2002@gmail.com>
2022-09-14 16:21:16 -0700

    Debugging in C

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
 	int leap;
@@ -31,19 +30,14 @@ void print_remaining_days(int month, int day, int year)
 	if (leap)
 	{
 		if (day > 59)
-		{
 			day++;
-		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
-		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
