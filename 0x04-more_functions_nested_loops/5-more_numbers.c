2594e9d5dfdb2567da47ce18b376650d971094bf
mrprinceikpa <obinichi2002@gmail.com>
2022-09-15 12:41:34 -0700

    C project- more on functions and nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *more_numbers - print digits 0 - 14, ten times
+ *Return: 0 on completion
+ **/
+void more_numbers(void)
+{
+	int times, digits, i, breaker;
+
+	digits = 1;
+	breaker = 58;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (i = 48; i < breaker; i++)
+		{
+			if (digits == 1)
+			{
+				breaker = 52;
+				_putchar(49);
+			}
+			_putchar(i);
+			if (i == 57)
+				i = 48;
+				digits++;
+		}
+		_putchar(10);
+	}
+}
