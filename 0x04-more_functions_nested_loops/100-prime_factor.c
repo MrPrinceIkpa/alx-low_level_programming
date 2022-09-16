7aa646afe1be97d67a21273d49544f9a235db1f8
mrprinceikpa <obinichi2002@gmail.com>
2022-09-15 17:45:30 -0700

    C project- moe on functions and nestes loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ *main - prints the largest prime factor of the number 612852475143
+ *Return - 0 on completion
+ */
+int main(void)
+{
+	unsigned long int tint, sqtint;
+
+	tint = 612852475143;
+	sqtint = sqrt(tint);
+
+	for (sqtint; sqtint > 2; sqtint--)
+	{
+		if (tint % sqtint == 0)
+			printf("%lu", sqtint);
+		break;
+	}
+	return (0);
+}
