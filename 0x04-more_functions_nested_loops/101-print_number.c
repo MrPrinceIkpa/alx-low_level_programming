a842548bd2f5628793a564d3042634a5ba192940
mrprinceikpa <obinichi2002@gmail.com>
2022-09-15 18:10:30 -0700

    C project- more on functions and nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int tint;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		tint = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
 	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	else
+		tint = n;
+	if (tint / 10)
+		print_number(tint / 10);
+	_putchar((tint % 10) + '0');
 }
