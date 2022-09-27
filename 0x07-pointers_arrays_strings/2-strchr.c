4357209161006ac04d3b0d66525c10891f502a9f
mrprinceikpa <obinichi2002@gmail.com>
2022-09-26 19:07:27 -0700

    C project- double pointers and multi-dimensional arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strchr - locates a character in a string
+ *@s: string to search
+ *@c: the character to search
+ *Return: the pointer to the first occurence of the string
+ */
+char *_strchr(char *s, char c)
+{
+	int i, lidx;
+
+	lidx = -1;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			lidx = i;
+			break;
+		}
+	}
+
+	if (lidx < 0)
+		return (NULL);
+	else
+		return (s + lidx);
+}
