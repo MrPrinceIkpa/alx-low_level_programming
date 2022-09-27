3a44e5d8a97d0947a88b33e30f15eebd081894fe
mrprinceikpa <obinichi2002@gmail.com>
2022-09-26 18:23:01 -0700

    C project- double pointers and multi-dimensional arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,10 +7,10 @@
  *@n: extension from the starting location
  *Return: a pointer to the memory area @s
  */
-char *_memset(char *s, char b, unsigned int n)
+char **_memset(char *s, char b, unsigned int n)
 {
 	char **ptr_s =  &s;
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		*(s + i) = b;
