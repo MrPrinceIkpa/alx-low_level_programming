af86da20d3d6dafeae678b4cebbb65cf937f5e25
mrprinceikpa <obinichi2002@gmail.com>
2022-10-11 10:03:33 -0700

    C project- preprocessor directives

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of program
+ *Return: 0 on completion
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+C project- preprocessor directives
