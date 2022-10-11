df1f660512d385afc9afb50f384866214f5c9797
mrprinceikpa <obinichi2002@gmail.com>
2022-10-11 12:27:10 -0700

    C project- structures and typedef: Correction 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
 
 /**
- *init_dog - initializes a structure datatype named dog
- *@d: pointer used for initialisaition
- *@name: a char variable under the structure
- *@age: a float variable under the structure
- *@owner: a char variable under the structure
- *Return: void
+ * init_dog - initialize a variable of type `struct dog`
+ * @d: struct dog
+ * @name: string for name
+ * @age: integer for age
+ * @owner: string for owners name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog tmp
+	if (d != NULL)
 	{
-		char *name;
-		float age;
-		char *owner;
-	};
-	d = &tmp;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
