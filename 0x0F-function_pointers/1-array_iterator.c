99f74dc54fef90a037aa243d19366c78bee0bf70
mrprinceikpa <obinichi2002@gmail.com>
2022-10-12 19:07:17 -0700

    C project- pointer to functions: Correction 7

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,21 @@
+#include <stdio.h>
 #include "function_pointers.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- *array_iterator -executes a function for each element of array
- *@array: The array to loop through
- *@size: size of the array
- *@action: a pointer to the function to be executed
- *Return: void
+ * array_iterator - iterates through array
+ * @array: array to go through
+ * @size: size of array
+ * @action: action function to take
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i, nelem = size / (sizeof(int));
+	unsigned int i;
 
-	if (!array || !action || size )
+	if (array == NULL || action == NULL)
 		exit(98);
-
-	for (i = 0; i < nelem; i++)
-		(*action)(array[i]);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
+
