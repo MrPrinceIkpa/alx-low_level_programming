6f7fc07156273ce2c5d7b7d010d3a6d96038bd9d
mrprinceikpa <obinichi2002@gmail.com>
2022-10-13 19:20:43 -0700

    C project- Variadic Functions: Correction 6

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,25 +1,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-/**
- *_copystr - copies string from one location to another
- *@mstr: the string to copy
- *Return: a pointer to the new location
- */
-char *_copystr(char *mstr)
-{
-	char *add;
-	int i;
 
-	for (i = 0; mstr[i] != '\0'; i++)
-		;
-	add = malloc(sizeof(char) * (i + 1));
-	for (i = 0; mstr[i] != '\0'; i++)
-		add[i] = mstr[i];
-	add[i + 1] = '\0';
-	return (add);
-}
 /**
  *print_strings - prints strings followed by a new line
  *@seperator: a string to seperate the strings
@@ -37,17 +19,14 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 	va_start(trn, n);
 	for (i = 0; i < n; i++)
 	{
-		tmp_str = _copystr(va_arg(trn, char *));
+		tmp_str = va_arg(trn, char *);
 		if (!tmp_str)
 			printf("(nil)");
 		else
 			printf("%s", tmp_str);
-		free(tmp_str);
-		if (i != n - 1)
+		if (i != (n - 1))
 		{
-			if (!seperator)
-				continue;
-			else
+			if (seperator)
 				printf("%s", seperator);
 		}
 	}
